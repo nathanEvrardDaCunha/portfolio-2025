cd254121a1e5541620783d8e48f2989c29db9de3
EVRARD Nathan <nathan.evrarddacunha@proton.me>
2025-12-09 16:28:50 +0100

    chore: install tailwind to style project website

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 
 function App() {
   // TODO: Use proper html semantic structure with <h1>, <h2>...
-  return <></>;
+  return (
+    <>
+      <h1 className="text-gray-800 text-6xl font-bold">Hello world</h1>
+    </>
+  );
 }
 
 export default App;
